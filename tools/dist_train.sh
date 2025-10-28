2eacf4bba9f86c56e1c04ac23f2fcc0dfacdd6d4
esat <esatkalfaoglu@gmail.com>
2025-10-28 13:09:18 +0300

    dist train updated

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -9,6 +9,6 @@ CONFIG=projects/configs/toponet_r50_8x1_24e_olv2_subset_A_near.py
 GPUS=$1
 PORT=${PORT:-28510}
 
-python -m torch.distributed.run --nproc_per_node=$GPUS --master_port=$PORT \
+~/containers/python_topomlp -m torch.distributed.run --nproc_per_node=$GPUS --master_port=$PORT \
     tools/train.py $CONFIG --launcher pytorch --work-dir ${WORK_DIR} --deterministic ${@:2} \
     2>&1 | tee ${WORK_DIR}/train.${timestamp}.log
