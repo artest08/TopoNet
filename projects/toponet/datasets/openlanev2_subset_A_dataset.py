cf05192ddb2430bdf743866b5330f6a4e52d74db
esat <esatkalfaoglu@gmail.com>
2025-10-28 13:02:20 +0300

    near split and my eval

--- projects/toponet/datasets/openlanev2_subset_A_dataset.py
+++ projects/toponet/datasets/openlanev2_subset_A_dataset.py
@@ -17,7 +17,8 @@ from pyquaternion import Quaternion
 from mmcv.parallel import DataContainer as DC
 from mmdet.datasets import DATASETS
 from mmdet3d.datasets import Custom3DDataset
-from openlanev2.evaluation import evaluate as openlanev2_evaluate
+# from openlanev2.evaluation import evaluate as openlanev2_evaluate
+from projects.openlanev2_evaluation.evaluate_v11 import evaluate as openlanev2_evaluate
 from openlanev2.utils import format_metric
 from openlanev2.visualization import draw_annotation_pv, assign_attribute, assign_topology
 
