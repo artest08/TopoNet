ddbb53ad4671196e022132046f3d027803290c4e
esat <esatkalfaoglu@gmail.com>
2025-11-01 17:37:56 +0300

    near without eval now

--- projects/configs/toponet_r50_8x1_24e_olv2_subset_A_near.py
+++ projects/configs/toponet_r50_8x1_24e_olv2_subset_A_near.py
@@ -327,9 +327,8 @@ lr_config = dict(
     min_lr_ratio=1e-3)
 total_epochs = 24
 evaluation = dict(
-    interval=1, 
+    interval=24, 
     pipeline=test_pipeline,
-    tmpdir='.dist_eval',
 )
 
 runner = dict(type='EpochBasedRunner', max_epochs=total_epochs)
@@ -340,7 +339,7 @@ log_config = dict(
         dict(type='TensorboardLoggerHook')
     ])
 
-checkpoint_config = dict(interval=1, max_keep_ckpts=1)
+checkpoint_config = dict(interval=1, max_keep_ckpts=5)
 
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
