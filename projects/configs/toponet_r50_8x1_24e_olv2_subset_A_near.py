09757253292fade67b4faf4d1130ad946d3d0979
esat <esatkalfaoglu@gmail.com>
2025-11-01 14:35:06 +0300

    eval trial config change

--- projects/configs/toponet_r50_8x1_24e_olv2_subset_A_near.py
+++ projects/configs/toponet_r50_8x1_24e_olv2_subset_A_near.py
@@ -326,11 +326,15 @@ lr_config = dict(
     warmup_ratio=1.0 / 3,
     min_lr_ratio=1e-3)
 total_epochs = 24
-evaluation = dict(interval=1, pipeline=test_pipeline)
+evaluation = dict(
+    interval=1, 
+    pipeline=test_pipeline,
+    tmpdir='.dist_eval',
+)
 
 runner = dict(type='EpochBasedRunner', max_epochs=total_epochs)
 log_config = dict(
-    interval=1000,
+    interval=3000,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='TensorboardLoggerHook')
